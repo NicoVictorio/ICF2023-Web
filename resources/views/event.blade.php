44e7b08252d2386a566913c87921f5ee21d5b706
tygarean <nico.victorio@gmail.com>
2023-09-18 11:33:59 +0700

    update event

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -18,8 +18,29 @@
         height: 600px;
         background: transparent;
         border: 2px solid white;
-        border-radius: 10%;
+        border-radius: 5%;
         backdrop-filter: blur(40px);
+    }
+
+    .container-event .seminar .seminar-cards .label .free{
+        border: 1px solid white;
+        border-radius: 20%;
+        padding: 5px;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .container-event .seminar .seminar-cards .label .certificate{
+        border: 1px solid white;
+        border-radius: 20%;
+    }
+
+    .container-event .seminar .seminar-cards .label img {
+        position: relative;
+        height: 30px;
     }
 
     .container-event .competitions-cards {
@@ -34,7 +55,31 @@
             </div>
             <br><br>
             <div class="seminar-cards">
-
+                <div class="row w-100 p-5 m-0">
+                    <div class="label">
+                        <div class="row">
+                            <div class="col-md-2 free">
+                                <img src="{{ asset('assets') }}/img/free.png">
+                                <a style="color: white;">Free</a>
+                            </div>
+                            <div class="col-md-3 certificate">
+                                <img src="{{ asset('assets') }}/img/certificate.png">
+                                <a style="color: white;">E-Certificate</a>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="judul">
+                    </div>
+                    <div class="description">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="registration">
+                        <button>Registration</button>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="competition m-0 p-0">
